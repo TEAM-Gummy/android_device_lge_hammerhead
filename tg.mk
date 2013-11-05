8216170ec82ac4221305752b6127c4a194d093fd
papi92 <papi92@droid-hive.com>
2013-11-05 00:15:43 -0500

    Gummified and got rid of prebuilt

---
+++ tg.mk
@@ -0,0 +1,23 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/Gummy/config/gsm.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/lge/mako/full_hammerhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
+PRODUCT_NAME := tg_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
